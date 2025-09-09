6a2f0ffdb50603636c580bf908f95231ef24a44e
Your Name <your.email@example.com>
2025-09-10 01:29:35 +0300

    Add CarRegistration class for managing car registration details and update workspace.xml for task management improvements.

--- functions/car_registration.py
+++ functions/car_registration.py
@@ -1,5 +1,59 @@
 from functions.pages import navigate_to_employees_page
 
+
+class CarRegistration:
+    """
+    A class to represent a car registration with license plate and year.
+    """
+    
+    def __init__(self, license_plate="", year=None):
+        """
+        Initialize a CarRegistration object.
+        
+        Args:
+            license_plate (str): The license plate number
+            year (int): The year of the car
+        """
+        self._license_plate = license_plate
+        self._year = year
+    
+    def get_license_plate(self):
+        """
+        Get the license plate number.
+        
+        Returns:
+            str: The license plate number
+        """
+        return self._license_plate
+    
+    def set_license_plate(self, license_plate):
+        """
+        Set the license plate number.
+        
+        Args:
+            license_plate (str): The license plate number to set
+        """
+        self._license_plate = license_plate
+    
+    def get_year(self):
+        """
+        Get the year of the car.
+        
+        Returns:
+            int: The year of the car
+        """
+        return self._year
+    
+    def set_year(self, year):
+        """
+        Set the year of the car.
+        
+        Args:
+            year (int): The year to set
+        """
+        self._year = year
+
+
 def validate_year_list(driver, year_list):
     """
     Validate a list of years in the car registration year dropdown.
@@ -10,9 +64,5 @@ def validate_year_list(driver, year_list):
     """
     page = navigate_to_employees_page(driver)
     page.validate_option_year_list(year_list)
-
-
-
-
 
 
