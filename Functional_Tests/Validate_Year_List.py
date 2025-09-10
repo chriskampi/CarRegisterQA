9a9b908c976aad10bdfdc1fae4b2b5da8bf8b0f0
Your Name <your.email@example.com>
2025-09-10 10:41:26 +0300

    Renaming Folder for tests

--- Functional_Tests/Validate_Year_List.py
+++ Functional_Tests/Validate_Year_List.py
@@ -1,12 +1,13 @@
 import pytest
-from functions.car_registration import validate_year_list
+from functions.car_registration import CarRegistration
 from values_data.years import car_registration_option_years
 
 @pytest.mark.browser
 class TestBrowserFunctionality:
     """Test class for browser-based testing of the HTML file"""
     YEAR_OPTIONS = car_registration_option_years
+    CAR_REGISTRATION = CarRegistration()
 
     def test_page_loads_successfully(self, setup_browser_test):
         """Test that the HTML page loads without errors"""
-        validate_year_list(setup_browser_test, self.YEAR_OPTIONS)
+        self.CAR_REGISTRATION.validate_year_list(setup_browser_test, self.YEAR_OPTIONS)
